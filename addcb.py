25a849d1ee58d8eda0111b7d6d66f9337471e67b
Jon <stricoff92@gmail.com>
2022-06-10 15:20:10 -0400

    wip

--- addcb.py
+++ addcb.py
@@ -16,26 +16,31 @@ if __name__ == "__main__":
     js_patt = re.compile(r'src=\"js\/[a-z_]+\.js\"')
     css_patt = re.compile(r'href=\"css\/[a-z_]+\.css\"')
 
-    with open("build/index.html") as rh:
-        html = rh.read()
+    index_files = [
+        "build/final-approach-2/index.html",
+    ]
 
-    outText = StringIO()
+    for index_file in index_files:
+        with open(index_file) as rh:
+            html = rh.read()
 
-    for line in html.split("\n"):
-        js_m = js_patt.search(line)
-        css_m = css_patt.search(line)
-        if js_m:
-            html_param = js_m.group()
-            new_line = line.replace(html_param, add_param(html_param, cb_value))
-            outText.write(new_line + "\n")
-            print("adding CB to line " + line.replace(' ', ''))
-        elif css_m:
-            html_param = css_m.group()
-            new_line = line.replace(html_param, add_param(html_param, cb_value))
-            outText.write(new_line + "\n")
-            print("adding CB to line " + line.replace(' ', ''))
-        else:
-            outText.write(line + "\n")
+        outText = StringIO()
 
-    with open("build/index.html", "w") as wh:
-        wh.write(outText.getvalue())
+        for line in html.split("\n"):
+            js_m = js_patt.search(line)
+            css_m = css_patt.search(line)
+            if js_m:
+                html_param = js_m.group()
+                new_line = line.replace(html_param, add_param(html_param, cb_value))
+                outText.write(new_line + "\n")
+                print("adding CB to line " + line.replace(' ', ''))
+            elif css_m:
+                html_param = css_m.group()
+                new_line = line.replace(html_param, add_param(html_param, cb_value))
+                outText.write(new_line + "\n")
+                print("adding CB to line " + line.replace(' ', ''))
+            else:
+                outText.write(line + "\n")
+
+        with open(index_file, "w") as wh:
+            wh.write(outText.getvalue())
