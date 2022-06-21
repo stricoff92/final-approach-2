7a97d7fda2ba01932126e02b7d11b377c0653ac7
Jon <stricoff92@gmail.com>
2022-06-21 12:38:44 -0400

    wip

--- src/final-approach-2/js/camera.js
+++ src/final-approach-2/js/camera.js
@@ -1585,8 +1585,8 @@ function _drawCarrierRunway(state, nowTS) {
     let topLeftPlaneCorner, planeRowXPointer, planeRowYPointer;
     const planeXPX = state.plane.dimensions[noFlare][0] * mupm;
     const planeYPX = state.plane.dimensions[noFlare][1] * mupm;
-    const xInt = (0.75 * mupm);
-    const yInt = (0.9 * mupm)
+    const xInt = (0.85 * mupm);
+    const yInt = (0.85 * mupm);
 
     planeRowXPointer = state.map.rwP1MapCoord[0] + (5 * mupm);
     planeRowYPointer = boatMapTopLeft[1] - (0.5 * mupm);
