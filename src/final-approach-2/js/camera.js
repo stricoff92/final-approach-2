6b8709b47b21de7a7f4d2792f522c46979fcf72a
Jon <stricoff92@gmail.com>
2022-06-22 09:03:48 -0400

    wip

--- src/final-approach-2/js/camera.js
+++ src/final-approach-2/js/camera.js
@@ -297,7 +297,6 @@ function drawGameScene(state) {
         const showVisualSkake = isTireStrike || isAAFire;
         let xAmnt = 0, yAmnt = 0;
         if(showVisualSkake) {
-            console.log({shakeMeters: lastTireStrike.shakeMeters})
             if(isTireStrike) {
                 xAmnt = (showVisualSkake ? getRandomFloat(-1 * lastTireStrike.shakeMeters, lastTireStrike.shakeMeters) * state.map.mapUnitsPerMeter : 0);
                 yAmnt = (showVisualSkake ? getRandomFloat(-1 * lastTireStrike.shakeMeters, lastTireStrike.shakeMeters) * state.map.mapUnitsPerMeter : 0);
